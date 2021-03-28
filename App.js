0ffce0d9e5100ad53e8fec04294a6717d114e9b8
Wlad11 <dds11malkoff@yandex.ru>
2021-03-29 01:03:28 +0700

    Добавил location и проверку позиции

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 import Loading from './Loading';
 
-export default function App() {
+export default class extends React.Component {
+
+  getLocation = async () => {
+   const location = await Location.getCurrentPositionAsync();
+   console.log(location);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render () {
   return (
     <Loading />
     
-  );
+    );
+  }
 }
 
 
